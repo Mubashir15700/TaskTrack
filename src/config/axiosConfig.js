96efac028500d13ed22ced1e518b8fd062a84ed2
Mubashir15700 <mubashir15700@gmail.com>
2024-02-22 13:10:27 +0530

    ready to host

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = import.meta.env.VITE_AXIOS_BASE_URL;
 
 export default axios;
