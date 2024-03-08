6d945511e3058eb90398b244f3e6bdb99c4f0a36
Mubashir15700 <mubashir15700@gmail.com>
2024-03-08 16:29:42 +0530

    change base url and build again

---
+++ dist/assets/laborer-f4930690.js
@@ -0,0 +1,1 @@
+import{h as s,b as e}from"./index-5c5faa49.js";const n=async(a,r,o=null,t=null)=>s(e.get(`/laborers/${a}/${r}`,{params:{lat:o,lon:t}})),b=async a=>s(e.get(`/laborers/${a}`));export{n as a,b as g};
