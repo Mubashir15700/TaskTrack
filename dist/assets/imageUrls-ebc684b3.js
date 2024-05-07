fd3e25aa4e56f040bdc307b5c6ec2d94b19c2606
Mubashir15700 <mubashir15700@gmail.com>
2024-05-07 20:54:15 +0530

    build after home page contents updated

--- dist/assets/imageUrls-ebc684b3.js
+++
@@ -1,1 +0,0 @@
-const t={avatar:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp",chatUser:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"};export{t as I};
