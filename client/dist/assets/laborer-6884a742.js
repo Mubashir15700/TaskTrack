589de839345ec4848dbc9780071db679de86b0ec
Mubashir15700 <mubashir15700@gmail.com>
2024-07-27 08:45:38 +0530

    feat: cicd completed

--- client/dist/assets/laborer-6884a742.js
+++
@@ -1,1 +0,0 @@
-import{h as s,b as e}from"./index-4386c688.js";const n=async(a,r,o=null,t=null)=>s(e.get(`/laborers/${a}/${r}`,{params:{lat:o,lon:t}})),b=async a=>s(e.get(`/laborers/${a}`));export{n as a,b as g};
